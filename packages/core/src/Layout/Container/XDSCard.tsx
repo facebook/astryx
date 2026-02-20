740c1efddffa525875559bc6a7283cd4753afc8d
cixzhang <cixzhang@gmail.com>
2026-02-20 17:19:27 +0000

    cleanup: simplify Layout/Container re-export of XDSCard
    Co-authored-by: Navi <navi@multimango.com>

--- packages/core/src/Layout/Container/XDSCard.tsx
+++ packages/core/src/Layout/Container/XDSCard.tsx
@@ -1,11 +1,5 @@
 /**
- * @file XDSCard.tsx
- * @input Re-exports from Card module
- * @output Exports XDSCard component and types
- * @position Backward-compatible re-export; canonical source is /packages/core/src/Card/
- *
- * @deprecated Import from '@xds/core/Card' instead of '@xds/core/Layout'.
+ * @deprecated Import from '@xds/core/Card' instead.
  */
-
 export {XDSCard} from '../../Card/XDSCard';
 export type {XDSCardProps, SizeValue} from '../../Card/XDSCard';
