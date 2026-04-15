3443adc9de33a9c407f92a5309763aee150e4aad
Joey Farina <joeyfarina@meta.com>
2026-04-15 12:25:35 -0700

    feat(blocks): migrate Skeleton examples to block templates (4 blocks)

---
+++ packages/cli/templates/blocks/components/Skeleton/SkeletonBasicTextPlaceholder.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import {XDSSkeleton} from '@xds/core/Skeleton';
+
+export default function SkeletonBasicTextPlaceholder() {
+  return (
+    <XDSSkeleton width={200} height={16} />
+  );
+}
