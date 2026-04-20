dfb0f0eb2cc247fa7c121e8cca4c24b5dca9bd66
Ted McDonald <tedmcdo@gmail.com>
2026-04-20 15:25:56 -0700

    feat(templates): add Table Page template (#1535)
    Data table page with power search filtering and action toolbar,
    using 38 dog breed entries as realistic mock data.
    
    Co-authored-by: Theodore Mcdonald <tedmcdo@fb.com>

---
+++ packages/cli/templates/pages/table-page/template.doc.mjs
@@ -0,0 +1,8 @@
+/** @type {import('../../../../core/src/docs-types').TemplateDoc} */
+export const doc = {
+  type: 'page',
+  name: 'Table Page',
+  description:
+    'Data table page with power search filtering and action toolbar.',
+  isReady: false,
+};
