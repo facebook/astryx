6639917463d1410198b1d5a167fb41204a970816
Navi <navi@navibot.dev>
2026-04-21 22:47:42 +0000

    Stack TextColors labels above text to match TextHeadingLevels

--- packages/cli/templates/blocks/components/Text/TextColors.tsx
+++ packages/cli/templates/blocks/components/Text/TextColors.tsx
@@ -15,7 +15,7 @@ export default function TextColors() {
   return (
     <XDSStack direction="vertical" gap={3}>
       {COLORS.map(({color, description}) => (
-        <XDSStack key={color} direction="horizontal" gap={3} vAlign="center">
+        <XDSStack key={color} direction="vertical" gap={0}>
           <XDSText type="supporting" color="secondary">
             {color}
           </XDSText>
