0dac7349e4810ea38ccb7d25ea5f1c708ce5f72c
rubyycheung <254546175+rubyycheung@users.noreply.github.com>
2026-04-23 03:29:38 -0500

    fix: add DateString type to useState in DateInputShowcase

--- packages/cli/templates/blocks/components/DateInput/DateInputShowcase.tsx
+++ packages/cli/templates/blocks/components/DateInput/DateInputShowcase.tsx
@@ -5,6 +5,9 @@ import {XDSDateInput} from '@xds/core/DateInput';
 import {XDSStack} from '@xds/core/Layout';
 import * as stylex from '@stylexjs/stylex';
 
+type DateString =
+  `${number}${number}${number}${number}-${number}${number}-${number}${number}`;
+
 const styles = stylex.create({
   root: {
     width: 320,
@@ -12,7 +15,7 @@ const styles = stylex.create({
 });
 
 export default function DateInputShowcase() {
-  const [date, setDate] = useState(undefined);
+  const [date, setDate] = useState<DateString | undefined>(undefined);
 
   return (
     <XDSStack direction="vertical" xstyle={styles.root}>
