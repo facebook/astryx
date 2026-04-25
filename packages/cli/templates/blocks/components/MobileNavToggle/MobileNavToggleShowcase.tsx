c2ed03b4a2271d85c222249767708bff4c907189
Navi <navi@navibot.dev>
2026-04-25 22:59:35 +0000

    fix: use real XDSMobileNavToggle inside AppShell with hasToggle:false and lg breakpoint

--- packages/cli/templates/blocks/components/MobileNavToggle/MobileNavToggleShowcase.tsx
+++ packages/cli/templates/blocks/components/MobileNavToggle/MobileNavToggleShowcase.tsx
@@ -1,38 +1,30 @@
 'use client';
 
-import {useState} from 'react';
-import {XDSMobileNav} from '@xds/core/MobileNav';
+import {XDSAppShell} from '@xds/core/AppShell';
+import {XDSMobileNavToggle} from '@xds/core/MobileNav';
 import {XDSSideNavItem, XDSSideNavSection} from '@xds/core/SideNav';
-import {XDSButton} from '@xds/core/Button';
-import {XDSIcon} from '@xds/core/Icon';
 import {XDSCenter} from '@xds/core/Center';
 import {XDSVStack} from '@xds/core/Layout';
 import {XDSText} from '@xds/core/Text';
 
 export default function MobileNavToggleShowcase() {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
-    <XDSCenter>
-      <XDSVStack gap={3} hAlign="center">
-        <XDSButton
-          label="Open navigation"
-          variant="ghost"
-          icon={<XDSIcon icon="menu" color="inherit" />}
-          isIconOnly
-          onClick={() => setIsOpen(true)}
-        />
-        <XDSText type="supporting" color="secondary">
-          XDSMobileNavToggle renders this hamburger button automatically inside
-          an AppShell at mobile breakpoints.
-        </XDSText>
-        <XDSMobileNav isOpen={isOpen} onOpenChange={setIsOpen} header="Navigation">
-          <XDSSideNavSection title="Pages">
-            <XDSSideNavItem label="Home" isSelected href="#" />
-            <XDSSideNavItem label="Settings" href="#" />
-          </XDSSideNavSection>
-        </XDSMobileNav>
-      </XDSVStack>
-    </XDSCenter>
+    <XDSAppShell
+      mobileNav={{hasToggle: false, breakpoint: 'lg'}}
+      sideNav={
+        <XDSSideNavSection title="Pages">
+          <XDSSideNavItem label="Home" isSelected href="#" />
+          <XDSSideNavItem label="Settings" href="#" />
+        </XDSSideNavSection>
+      }>
+      <XDSCenter>
+        <XDSVStack gap={3} hAlign="center">
+          <XDSMobileNavToggle />
+          <XDSText type="supporting" color="secondary">
+            Resize below the large breakpoint to see the toggle
+          </XDSText>
+        </XDSVStack>
+      </XDSCenter>
+    </XDSAppShell>
   );
 }
