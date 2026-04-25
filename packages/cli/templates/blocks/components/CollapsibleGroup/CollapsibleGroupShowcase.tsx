12965d4b76264502211e8db8696daa10ee5c58ea
Navi <navi@navibot.dev>
2026-04-25 22:56:34 +0000

    fix: add width 100% to VStack in CollapsibleGroup

--- packages/cli/templates/blocks/components/CollapsibleGroup/CollapsibleGroupShowcase.tsx
+++ packages/cli/templates/blocks/components/CollapsibleGroup/CollapsibleGroupShowcase.tsx
@@ -10,7 +10,7 @@ export default function CollapsibleGroupShowcase() {
   return (
     <XDSCenter width={400}>
       <XDSCollapsibleGroup type="single" defaultValue="shipping">
-        <XDSVStack gap={2}>
+        <XDSVStack gap={2} style={{width: '100%'}}>
           <XDSCard>
             <XDSCollapsible trigger="Shipping Information" value="shipping">
               <XDSText type="body" color="secondary">
