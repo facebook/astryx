d37acde39c309657ee7b944db5136f98a04af453
saivazian <saivazian@meta.com>
2026-04-29 23:25:26 +0000

    fix(centered-hero): rename nav heading to OCEAN

--- packages/cli/templates/pages/centered-hero/page.tsx
+++ packages/cli/templates/pages/centered-hero/page.tsx
@@ -43,7 +43,7 @@ export default function CenteredHero() {
       topNav={
         <XDSTopNav
           label="Main navigation"
-          heading={<XDSTopNavHeading heading="DAIILY" />}
+          heading={<XDSTopNavHeading heading="OCEAN" />}
           endContent={
             <>
               <XDSTopNavItem label="Products" href="#" />
