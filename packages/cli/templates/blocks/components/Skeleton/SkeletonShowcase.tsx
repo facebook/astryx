3556300ea02affafb04412b4db02612a568634c1
Kenton Daniel Quatman <kenton@meta.com>
2026-05-04 18:41:49 -0400

    Skeleton

--- packages/cli/templates/blocks/components/Skeleton/SkeletonShowcase.tsx
+++ packages/cli/templates/blocks/components/Skeleton/SkeletonShowcase.tsx
@@ -1,7 +1,23 @@
 'use client';
 
 import {XDSSkeleton} from '@xds/core/Skeleton';
+import {XDSHStack, XDSVStack} from '@xds/core/Layout';
 
 export default function SkeletonShowcase() {
-  return <XDSSkeleton width={200} height={20} radius={3} />;
+  return (
+    <XDSVStack gap={4} width={300}>
+      <XDSHStack gap={4} vAlign="center">
+        <XDSSkeleton width={64} height={64} radius="rounded" index={0} />
+        <XDSVStack gap={2}>
+          <XDSSkeleton width={160} height={20} index={1} />
+          <XDSSkeleton width={80} height={16} index={2} />
+        </XDSVStack>
+      </XDSHStack>
+      <XDSVStack gap={2}>
+        <XDSSkeleton width="90%" height={16} index={3} />
+        <XDSSkeleton width="100%" height={16} index={4} />
+        <XDSSkeleton width="75%" height={16} index={5} />
+      </XDSVStack>
+    </XDSVStack>
+  );
 }
